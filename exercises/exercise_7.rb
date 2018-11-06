ba0acc60e12a5d7a8d28bbcf37b5b1670c0ff209
Jenny Poon <jenny.poon@live.ca>
2018-11-06 22:09:13 +0000

    testing

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+print "What's your store's name? "
+new_store_name = gets.chomp
+
+@new_store = Store.create(name: new_store_name, mens_apparel: false)
+
+if @new_store.errors.any?
+  @new_store.errors.each do |attribute, message|
+    puts "#{attribute}: #{message}"
+  end
+end
