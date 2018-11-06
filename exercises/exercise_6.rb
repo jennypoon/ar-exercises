6acca19c5ccb0e5b2bb652defb0f06cdb3ae11db
Jenny Poon <jenny.poon@live.ca>
2018-11-06 21:12:49 +0000

    added employees to Store 1 and Store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 60)
+@store1.employees.create(first_name: "Ron", last_name: "Weasley", hourly_rate: 60)
+@store1.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 60)
+@store1.employees.create(first_name: "Hedwig", last_name: "Owlet", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Albus", last_name: "Dumbledore", hourly_rate: 100)
+@store2.employees.create(first_name: "Dobby", last_name: "Elf", hourly_rate: 40)
+@store2.employees.create(first_name: "Sirius", last_name: "Black", hourly_rate: 80)
+@store2.employees.create(first_name: "Remus", last_name: "Lupin", hourly_rate: 80)
+
+puts "Number of Employees in database: #{Employee.count}"
