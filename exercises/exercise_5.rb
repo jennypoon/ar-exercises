9907c2657f7fd389b8cdbe195142d0bd057f1457
Jenny Poon <jenny.poon@live.ca>
2018-11-06 21:05:06 +0000

    added annual and average revenue and counted number of stores that make more than avg

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,13 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@company_ann_rev = Store.sum(:annual_revenue)
+@company_avg_rev = @company_ann_rev/12
+
+puts "Total revenue for the entire company: $#{@company_ann_rev}"
+puts "Avg revenue for the entire company $#{@company_avg_rev}"
+
+@stores_above_avg = Store.where("annual_revenue > ?", @company_avg_rev).count
+
+
+puts "Number of stores making more than average #{@stores_above_avg}"
