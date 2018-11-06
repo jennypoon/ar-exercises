7833c2b2604a59c3abfb14c9960df70ac43d8a4a
Jenny Poon <jenny.poon@live.ca>
2018-11-06 20:16:24 +0000

    updated store name of 1 to Surrey

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+p @store1
+p @store2
+
+@store1.name = "Surrey"
+@store1.save
+
+puts "Changed store name of store1"
+p @store1
