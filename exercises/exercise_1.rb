aff50fe6e30c8aeab50f4dff14c34de5f871d285
Jenny Poon <jenny.poon@live.ca>
2018-11-06 20:08:17 +0000

    deleted class stores and changed to store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,11 +3,9 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-class Stores < ActiveRecord::Base
-end
 
-burnaby = Stores.create name: 'Burnaby', annual_revenue: '300000', mens_apparel: 'true', womens_apparel: 'true'
-richmond = Stores.create name: 'Richmond', annual_revenue: '1260000', mens_apparel: 'false', womens_apparel: 'true'
-gastown = Stores.create name: 'Gastown', annual_revenue: '190000', mens_apparel: 'true', womens_apparel: 'false'
+burnaby = Store.create name: 'Burnaby', annual_revenue: '300000', mens_apparel: 'true', womens_apparel: 'true'
+richmond = Store.create name: 'Richmond', annual_revenue: '1260000', mens_apparel: 'false', womens_apparel: 'true'
+gastown = Store.create name: 'Gastown', annual_revenue: '190000', mens_apparel: 'true', womens_apparel: 'false'
 
-puts "Number of Stores in database: #{Stores.count}"
+puts "Number of Stores in database: #{Store.count}"
